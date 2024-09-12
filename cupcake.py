ffb1525e0fef927f1975290d0f9fefac4ec302ea
info1 <nt879879@gmail.com>
2024-09-12 11:28:52 -0400

    dfhdfgjfd

---
+++ cupcake.py
@@ -0,0 +1,10 @@
+"""
+Programme qui te dit si tu paye trop cher pour ton cupcake.
+"""
+
+def cupcake_price_checker(p:float):
+    """
+    Vérifie si ton cupcake vaut la peine d'être acheté.
+    :param p: Prix
+    :return: None
+    """
